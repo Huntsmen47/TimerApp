fb29347af8b968838c89dfbafd89b475fd93925b
Hunter Larson <hunterlarson21@outlook.com>
2024-04-24 00:08:30 -0600

    Add timer application files

---
+++ Timer/pythonTimer.py
@@ -0,0 +1,45 @@
+import time
+import pygame
+
+def countdown_timer(seconds):
+    while seconds > 0:
+	    hours = seconds/3600
+	    minutes = seconds/60
+	    rounded_min = round(minutes,2)
+	    rounded_hour = round(hours,2)
+	    if hours >= 1:
+	    	print("Time remaining:",rounded_hour,"hours",rounded_min,"minutes", seconds, "seconds")
+	    elif minutes >= 1:
+	    	print("Time remaining:",rounded_min,"minutes", seconds, "seconds")
+	    else:
+	    	print("Time remaining:",seconds, "seconds")
+	    time.sleep(1)
+	    seconds -= 1
+
+def start_timer():
+    hours = int(input("Enter hours: ")) 
+    mins = int(input("Enter minutes: "))
+    seconds = int(input("Enter seconds: "))
+    hour_sec = 3600*hours
+    min_sec = 60*mins
+    total = min_sec + seconds + hour_sec
+    countdown_timer(total)
+    print("Timer finished!")
+    pygame.mixer.init()
+    pygame.mixer.music.load("/home/hunter/Desktop/Timer/beep.mp3")  # Replace "beep.wav" with your own sound file
+    pygame.mixer.music.play()
+
+# Main program loop
+while True:
+    print("\nSelect an option:")
+    print("1. Start Timer")
+    print("2. Exit")
+    choice = input("Enter your choice (1-2): ")
+
+    if choice == "1":
+        start_timer()
+    elif choice == "2":
+        break
+    else:
+        print("Invalid choice. Please try again.")
+
